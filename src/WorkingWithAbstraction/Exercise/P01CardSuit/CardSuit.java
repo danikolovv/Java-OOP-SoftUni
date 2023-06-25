86078ea4111129c9d3549083aff098e3f00a8782
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-25 15:51:59 +0300

    Initial commit

--- src/WorkingWithAbstraction/Exercise/P01CardSuit/CardSuit.java
+++ src/WorkingWithAbstraction/Exercise/P01CardSuit/CardSuit.java
@@ -1,4 +1,8 @@
 package WorkingWithAbstraction.Exercise.P01CardSuit;
 
 public enum CardSuit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
 }
--- src/WorkingWithAbstraction/Exercise/P01CardSuit/Main.java
+++ src/WorkingWithAbstraction/Exercise/P01CardSuit/Main.java
@@ -1,4 +1,13 @@
 package WorkingWithAbstraction.Exercise.P01CardSuit;
 
 public class Main {
+    public static void main(String[] args) {
+
+        CardSuit[] suits = CardSuit.values();
+
+        System.out.println("Card Suits: ");
+        for (CardSuit suit : suits) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", suit.ordinal(), suit.name());
+        }
+    }
 }
