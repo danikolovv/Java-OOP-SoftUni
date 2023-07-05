b9ad6b451b318852b7236d3b027128294c855326
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-07-05 14:11:27 +0300

    Initial commit

--- src/WorkingWithAbstraction/Exercise/P04TrafficLights/Main.java
+++ src/WorkingWithAbstraction/Exercise/P04TrafficLights/Main.java
@@ -2,22 +2,48 @@ package WorkingWithAbstraction.Exercise.P04TrafficLights;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Signals[] arrayColors = Arrays.stream(scanner.nextLine().split("\\s+"))
+        Signals[] signals = Arrays.stream(scanner.nextLine().split("\\s+"))
                                       .map(Signals::valueOf)
                                       .toArray(Signals[]::new);
 
-        Signals[] signals = Signals.values();
-
         int n = Integer.parseInt(scanner.nextLine());
-
         for (int i = 0; i < n; i++) {
-            
+
+            updateSignals(signals);
+
+            printSignals(signals);
         }
     }
+
+    private static void printSignals(Signals[] signals) {
+        for (Signals signal : signals) {
+            System.out.print(signal + " ");
+        }
+        System.out.println();
+    }
+
+    private static void updateSignals(Signals[] signals) {
+        // red -> green -> yellow -> red
+        for (int i = 0; i < signals.length; i++) {
+            switch (signals[i]) {
+                case RED:
+                    signals[i] = Signals.GREEN;
+                    break;
+                case GREEN:
+                    signals[i] = Signals.YELLOW;
+                    break;
+                case YELLOW:
+                    signals[i] = Signals.RED;
+                    break;
+            }
+        }
+    }
+
+
+
 }
